da5e148e4d7c4d9fa63fa1913cbfc4005a26c341
AlfaGruisPL <alfagruis@gmail.com>
2024-07-01 07:43:27 +0200

    work

--- src/components/home/header/Header.jsx
+++ src/components/home/header/Header.jsx
@@ -1,21 +1,22 @@
 import style from "./Header.module.scss";
-import backGround from "../../../assets/images/backgroundHeader.svg";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faAnglesDown } from "@fortawesome/free-solid-svg-icons";
+///import backGround from "../../../assets/images/backgroundHeader.svg";
+import backGround from "../../../assets/images/banner2.jpg";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faAnglesDown} from "@fortawesome/free-solid-svg-icons";
 
 function Header() {
-  return (
-    <div className={`wrapper ${style.container}`}>
-      <img src={backGround} alt="backGround" />
-      <h1>
-        AI drives <span>cybersecurity</span> and{" "}
-        <span>operational efficiency</span>, empowering you to achieve
-        <span> strategic organizational </span>
-        objectives.
-      </h1>
-      <FontAwesomeIcon icon={faAnglesDown} className={style.arrowIcon} />
-    </div>
-  );
+    return (
+        <div className={`wrapper ${style.container}`}>
+            <img src={backGround} alt="backGround"/>
+            <h1>
+                AI drives <span>cybersecurity</span> and{" "}
+                <span>operational efficiency</span>, empowering you to achieve
+                <span> strategic organizational </span>
+                objectives.
+            </h1>
+            <FontAwesomeIcon icon={faAnglesDown} className={style.arrowIcon}/>
+        </div>
+    );
 }
 
 export default Header;
